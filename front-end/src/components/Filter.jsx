6843064f0b16194ae52a395a8f7c03218ef53905
Jerry-K5050 <jerry5058@outlook.com>
2025-11-03 17:15:33 +0400

    changed the code format

--- front-end/src/components/Filter.jsx
+++ front-end/src/components/Filter.jsx
@@ -56,15 +56,15 @@ export default function Filter({
       <div className="flex items-center justify-between mb-6">
         <h3 className="text-lg font-semibold text-black">Ticker Filter</h3>
         <button
-  onClick={onToggleLock}
-  className={
-    locked
-      ? "tp-btn-lock-locked text-xs px-3 py-1"
-      : "tp-btn-white-outlined text-xs px-3 py-1"
-  }
->
-  {locked ? "Unlock" : "Lock"}
-</button>
+          onClick={onToggleLock}
+          className={
+          locked
+            ? "tp-btn-lock-locked text-xs px-3 py-1"
+            : "tp-btn-white-outlined text-xs px-3 py-1"
+          }
+        >
+          {locked ? "Unlock" : "Lock"}
+        </button>
       </div>
 
       <ul className="space-y-4">
