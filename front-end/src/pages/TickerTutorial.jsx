fc8c00c595e27c5d6d42575870818349817dce56
jlnsr <jls9980@nyu.edu>
2025-12-09 18:01:29 -0500

    Comitting new file

---
+++ front-end/src/pages/TickerTutorial.jsx
@@ -0,0 +1,39 @@
+/**
+ * Learn about each metric.
+ */
+
+/*
+P/E ratio: 
+Compares the price of the asset to how much it earns per share. Optimally, an investor would like to earn more per share, at a lower price per share. Therefore, lower P/E ratios are preferable. A higher P/E ratio means you are paying more for the same earning potential. 
+A lower P/E ratio may not necessarily be caused by increased earnings, but rather, a decline in price, which often reflects poorer sentiment towards the stock.
+The optimal range for `${metric}` (per sector):
+Tech / Growth: 20–40.
+Tech firms often justify high P/Es because of strong growth potential — rapid innovation, scalable business models, expanding future earnings.
+Defensive / Consumer Staples: 15 - 25. 
+These firms tend to have stable demand, steady cash flows, and lower growth — so a moderate P/E range reflects that stability without overpaying. A P/E much above 25× often implies investors expect acceleration or premium for safety. 
+Financials: 10–18.
+Financial firms typically have slower growth and more cyclical earnings, so lower P/Es make sense.  
+
+Market Capitalization:
+A generalization of a company’s total value.
+*/
+
+/*
+Beta: 
+Compares a stock’s price movement with the overall movement of the market. Stocks are volatile, which explains why a stock’s price chart is jagged. The market is likewise volatile, moving up and down. A stock’s beta tells you how much it moves relative to the market. 
+β = 1.0 indicates that the asset moves in line with the market, that is, the market performance is exactly indicative of the asset’s performance
+β < 1.0 indicates the asset is less volatile than the market
+β > 1.0 indicates the asset moves more than the market
+β < 0 indicates the asset moves inversely with the market.
+next Pushed code
+`{expression} indicates that the asset is `${as volatile as}` the market
+`{expression} indicates that the asset is `${less volatile than}` the market
+`{expression} indicates that the asset is `${more volatile than}` the market
+`{expression} indicates that the asset is `${inversely volatile with}` the market
+
+The optimal range for beta (per sector):
+Tech & consumer discretionary: 1.2 – 1.5
+Industrials & financials: 1.0
+Consumer staples & utilities: 0.4 – 0.8
+
+*/
